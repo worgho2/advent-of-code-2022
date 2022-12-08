f708e3cd3c3e7f0943c88b69b5931be0cb971d7f
Otávio Baziewicz Filho <otavio.baziewicz.filho@gmail.com>
2022-12-08 12:56:47 -0300

    Added 01 part 2

--- 01/script.sh
+++ 01/script.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-cat input.txt | tr '\n' '+' | sed 's/++/-/g' | tr '-' '\n' > parsed.txt
-echo '0' >> parsed.txt
-cat parsed.txt | bc | sort | tail -1
-rm -f parsed.txt
+cat input.txt | tr '\n' '+' | sed 's/++/-/g' | tr '-' '\n' > temp1
+echo '0' >> temp1
+cat temp1 | bc | sort | tail -3 | tr '\n' '+' > temp2
+echo '0' >> temp2
+
+FIRST=$(cat temp1 | bc | sort | tail -1)
+SECOND=$(cat temp2 | bc)
+
+echo -e "Highest sum: $FIRST\nTop three sum: $SECOND"
+
+rm -f temp1 temp2
