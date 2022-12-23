1854ae7637d049fb7fb6512b305b1284d250f2dd
Otávio Baziewicz Filho <otavio.baziewicz.filho@gmail.com>
2022-12-22 22:43:46 -0300

    Added 11

---
+++ 11/solution.mjs
@@ -0,0 +1,73 @@
+import fs from 'fs';
+import readline from 'readline';
+
+const lineReader = readline.createInterface({
+    input: fs.createReadStream('test-2.txt'),
+});
+
+const monkeys = [];
+let currentMonkey = {};
+
+for await (const line of lineReader) {
+    if (line.startsWith('Monkey')) {
+        currentMonkey = {};
+    }
+
+    if (line.startsWith('  Starting')) {
+        currentMonkey.items = line
+            .replace(',', '')
+            .split(' ')
+            .slice(4)
+            .map((i) => parseInt(i));
+    }
+
+    if (line.startsWith('  Operation')) {
+        currentMonkey.operation = (old) => eval(line.split(' ').slice(5).join(' '));
+    }
+
+    if (line.startsWith('  Test')) {
+        currentMonkey.test = {
+            divisor: parseInt(line.split(' ').slice(5)[0]),
+        };
+    }
+
+    if (line.startsWith('    If true')) {
+        currentMonkey.test.trueMokey = parseInt(line.split(' ').slice(9)[0]);
+    }
+
+    if (line.startsWith('    If false')) {
+        currentMonkey.test.falseMonkey = parseInt(line.split(' ').slice(9)[0]);
+        currentMonkey.inspectedItems = 0;
+        monkeys.push(currentMonkey);
+    }
+
+    if (line === '') {
+        currentMonkey = {};
+    }
+}
+
+for (let round = 0; round < 20; round++) {
+    for (let i = 0; i < monkeys.length; i++) {
+        for (let j = 0; j < monkeys[i].items.length; j++) {
+            monkeys[i].items[j] = monkeys[i].operation(monkeys[i].items[j]);
+            monkeys[i].items[j] = Math.floor(monkeys[i].items[j] / 3);
+            monkeys[i].inspectedItems += 1;
+
+            if (monkeys[i].items[j] % monkeys[i].test.divisor === 0) {
+                monkeys[monkeys[i].test.trueMokey].items.push(monkeys[i].items[j]);
+            } else {
+                monkeys[monkeys[i].test.falseMonkey].items.push(monkeys[i].items[j]);
+            }
+        }
+
+        monkeys[i].items = [];
+    }
+}
+
+const monkeyBusiness = monkeys
+    .map((a) => a.inspectedItems)
+    .sort((a, b) => b - a)
+    .slice(0, 2)
+    .reduce((a, b) => a * b);
+
+console.log(`Monkey business: ${monkeyBusiness}`);
